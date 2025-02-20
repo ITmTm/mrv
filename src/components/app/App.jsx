d30aa3bd4c5cf14ce9139bdb0d06f1ce765396af
mtm <Stafr95@mail.ru>
2025-02-20 20:43:00 +0500

    Wrapping components in ErrorBoundary

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,45 @@
+import { Component } from "react";
+
 import AppHeader from "../appHeader/AppHeader.jsx";
 import RandomChar from "../randomChar/RandomChar.jsx";
 import CharList from "../charList/CharList.jsx";
 import CharInfo from "../charInfo/CharInfo.jsx";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary.jsx";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+    state = {
+        selectedChar: null
+    }
+    
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
+                    <div className="char__content">
+                        <ErrorBoundary>
+                            <CharList onCharSelected={this.onCharSelected}/>
+                        </ErrorBoundary>
+                        
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
