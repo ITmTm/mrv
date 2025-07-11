f44b470e9e2945db1c857294ddce9b75dd877093
mtm <Stafr95@mail.ru>
2025-07-11 19:18:07 +0500

    fix

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,4 +1,3 @@
-
 import { useState, useCallback, } from "react";
 
 export const useHttp = () => {
@@ -10,10 +9,10 @@ export const useHttp = () => {
 		setLoading(true);
 
 		try {
-			const response = await fetch(url, {method, body, headers})
+			const response = await fetch(url, {method, body, headers});
 
 			if (!response.ok) {
-				throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+				throw new Error(`Could not fetch ${url}, status: ${response.status}`);
 			}
 
 			const data = await response.json();
