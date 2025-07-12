31afe3a032dfccc52843be6a00a55d3780999757
mtm <Stafr95@mail.ru>
2025-07-12 22:07:45 +0500

    changing indents

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,40 +1,45 @@
-import { useState } from "react";
+import {useState} from "react";
 
 import AppHeader from "../appHeader/AppHeader.jsx";
 import RandomChar from "../randomChar/RandomChar.jsx";
 import CharList from "../charList/CharList.jsx";
 import CharInfo from "../charInfo/CharInfo.jsx";
+import AppBanner from "../appBanner/AppBanner.jsx";
+import ComicsList from "../comicsList/ComicsList.jsx";
+
 import ErrorBoundary from "../errorBoundary/ErrorBoundary.jsx";
 
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
+  const [selectedChar, setChar] = useState(null);
 
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
+  const onCharSelected = (id) => {
+    setChar(id);
+  }
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
+  return (
+      <div className="app">
+        <AppHeader/>
+        <main>
+{/*          <ErrorBoundary>
+            <RandomChar/>
+          </ErrorBoundary>
+          <div className="char__content">
+            <ErrorBoundary>
+              <CharList onCharSelected={onCharSelected}/>
+            </ErrorBoundary>
 
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+            <ErrorBoundary>
+              <CharInfo charId={selectedChar}/>
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision"/>*/}
+          <AppBanner/>
+          <ComicsList/>
+        </main>
+      </div>
+  )
 }
 
 export default App;
