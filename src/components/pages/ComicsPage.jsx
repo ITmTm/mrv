e3137e44d068adebebcf14f082e709f1a2a8dad6
mtm <Stafr95@mail.ru>
2025-07-13 18:48:47 +0500

    splitting code into separate pages

---
+++ src/components/pages/ComicsPage.jsx
@@ -0,0 +1,13 @@
+import ComicsList from "../comicsList/ComicsList.jsx";
+import AppBanner from "../appBanner/AppBanner.jsx";
+
+const ComicsPage = () => {
+  return (
+      <>
+        <AppBanner/>
+        <ComicsList/>
+      </>
+  )
+}
+
+export default ComicsPage;
