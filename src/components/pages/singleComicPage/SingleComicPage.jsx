44b3f165f55d6e55beae0ec11e4699976577401d
mtm <Stafr95@mail.ru>
2025-07-14 16:44:55 +0500

    implementation of 404 error with a link back to the comics

--- src/components/pages/singleComicPage/SingleComicPage.jsx
+++ src/components/pages/singleComicPage/SingleComicPage.jsx
@@ -1,22 +1,71 @@
-import {Link} from "react-router-dom";
+import {useParams, Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
-import xMen from '../../../resources/img/x-men.png';
+import useMarvelService from "../../../services/MarvelService.js";
+
+import Spinner from "../../spinner/Spinner.jsx";
+import ErrorMessage from "../../errorMessage/ErrorMessage.jsx";
 
 import "./singleComicPage.scss";
 
 const SingleComicPage = () => {
+  const {comicId} = useParams();
+  const [comic, setComic] = useState(null);
+  const [notFound, setNotFound] = useState(false);
+
+  const {loading, error, getComic, clearError} = useMarvelService();
+
+  useEffect(() => {
+    setNotFound(false);
+    clearError();
+    setComic(null);
+
+    getComic(comicId)
+        .then(data => setComic(data))
+        .catch(() => setNotFound(true));
+  }, [comicId]);
+
+  if (notFound) {
+    return (
+        <div>
+          <ErrorMessage/>
+          <p className="error__message">Page doesn’t exist</p>
+          <Link className="error__message-back" to="/comics">
+            Back to comics
+          </Link>
+        </div>
+    );
+  }
+
+  const errorMessage = (error || notFound) && (
+      <ErrorMessage text={notFound ? "Comic not found" : undefined}/>
+  );
+  const spinner = loading && !comic && <Spinner/>;
+  const content = (!loading && !error && !notFound && comic) && (
+      <View comic={comic}/>
+  );
+
+  return (
+      <>
+        {errorMessage}
+        {spinner}
+        {content}
+      </>
+  )
+}
+
+const View = ({comic}) => {
+  const {title, description, pageCount, thumbnail, language, price} = comic;
+
   return (
       <div className="single-comic">
-        <img src={xMen} alt="x-men" className="single-comic__img"/>
+        <img src={thumbnail} alt={title} className="single-comic__img"/>
         <div className="single-comic__info">
-          <h2 className="single-comic__name">X-Men: Days of Future Past</h2>
-          <p className="single-comic__descr">Re-live the legendary first journey into the dystopian future of 2013 -
-            where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the
-            first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops
-            himself...and a demon for Christmas!?</p>
-          <p className="single-comic__descr">144 pages</p>
-          <p className="single-comic__descr">Language: en-us</p>
-          <div className="single-comic__price">9.99$</div>
+          <h2 className="single-comic__name">{title}</h2>
+          <p className="single-comic__descr">{description}</p>
+          <p className="single-comic__descr">{pageCount}</p>
+          <p className="single-comic__descr">Language: {language}</p>
+          <div className="single-comic__price">{price}</div>
         </div>
         <Link to="/comics" className="single-comic__back">Back to all</Link>
       </div>
