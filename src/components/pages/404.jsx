1df3df1c3c62457d65f6a974e762b734d25b8dfa
mtm <Stafr95@mail.ru>
2025-07-14 13:36:59 +0500

    creating a 404 error component

---
+++ src/components/pages/404.jsx
@@ -0,0 +1,16 @@
+import ErrorMessage from "../errorMessage/ErrorMessage.jsx";
+import {Link} from "react-router-dom";
+
+import "./404.scss"
+
+const Page404 = () => {
+  return (
+      <div>
+        <ErrorMessage/>
+        <p className="error__message">Page doesn't exist</p>
+        <Link className="error__message-back" to="/">Back to main page</Link>
+      </div>
+  )
+}
+
+export default Page404;
