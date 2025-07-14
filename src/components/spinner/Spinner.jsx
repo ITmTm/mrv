1be07c2bf3f038860b874a56934ce48cb62e3f9e
mtm <Stafr95@mail.ru>
2025-07-14 15:51:02 +0500

    change inline style for Spinner component

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,9 +1,9 @@
 import spinner from './spinner.gif'
 
 const Spinner = () => {
-	return (
-		<img src={spinner} alt='spinner' style={{ margin: '0 auto' }} />
-	)
+  return (
+      <img src={spinner} alt='spinner' style={{'display': 'block', 'margin': '0 auto'}}/>
+  )
 }
 
 export default Spinner;
