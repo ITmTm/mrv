cb0b57eda13785521eefe85c8b26bdba9071ad6b
mtm <Stafr95@mail.ru>
2025-07-14 15:11:43 +0500

    import component

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,6 @@
 import MainPage from "./MainPage.jsx";
 import ComicsPage from "./ComicsPage.jsx";
-import Page404 from "./404.jsx";
+import Page404 from "./404/404.jsx";
+import SingleComicPage from "./singleComicPage/SingleComicPage.jsx";
 
-export {MainPage, ComicsPage, Page404};
+export {MainPage, ComicsPage, Page404, SingleComicPage};
