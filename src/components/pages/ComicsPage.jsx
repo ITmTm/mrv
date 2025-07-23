d710a19bac1323a3c3ddd0dddae407a4c4fb31fd
mtm <Stafr95@mail.ru>
2025-07-23 18:53:06 +0500

    using meta tags and title in a component

--- src/components/pages/ComicsPage.jsx
+++ src/components/pages/ComicsPage.jsx
@@ -4,6 +4,9 @@ import AppBanner from "../appBanner/AppBanner.jsx";
 const ComicsPage = () => {
   return (
       <>
+        <meta name="description" content="Page with list of our comics"/>
+        <title>Comics page</title>
+
         <AppBanner/>
         <ComicsList/>
       </>
