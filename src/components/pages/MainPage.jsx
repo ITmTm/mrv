ec8625b37c29f2a7555d233e3456825f17d55036
mtm <Stafr95@mail.ru>
2025-07-23 17:25:05 +0500

    component import

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import RandomChar from "../randomChar/RandomChar.jsx";
 import CharList from "../charList/CharList.jsx";
 import CharInfo from "../charInfo/CharInfo.jsx";
+import CharSearchForm from "../charSearchForm/CharSearchForm.jsx";
 
 import ErrorBoundary from "../errorBoundary/ErrorBoundary.jsx";
 import decoration from '../../resources/img/vision.png';
@@ -24,9 +25,15 @@ const MainPage = () => {
             <CharList onCharSelected={onCharSelected}/>
           </ErrorBoundary>
 
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar}/>
-          </ErrorBoundary>
+          <div>
+            <ErrorBoundary>
+              <CharInfo charId={selectedChar}/>
+            </ErrorBoundary>
+
+            <ErrorBoundary>
+              <CharSearchForm/>
+            </ErrorBoundary>
+          </div>
         </div>
         <img className="bg-decoration" src={decoration} alt="vision"/>
       </>
