45191f40c4a0abd8c39295b67debd174ef1b65e1
mtm <Stafr95@mail.ru>
2025-07-23 18:53:39 +0500

    using meta tags and title in a component

--- src/components/pages/singleComicPage/SingleComicPage.jsx
+++ src/components/pages/singleComicPage/SingleComicPage.jsx
@@ -59,6 +59,9 @@ const View = ({comic}) => {
 
   return (
       <div className="single-comic">
+        <meta name="description" content={`${title} comics book`} />
+        <title>{title}</title>
+
         <img src={thumbnail} alt={title} className="single-comic__img"/>
         <div className="single-comic__info">
           <h2 className="single-comic__name">{title}</h2>
