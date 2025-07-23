833e1dce339038e7438e20334e0d5ec08c34954d
mtm <Stafr95@mail.ru>
2025-07-23 18:52:52 +0500

    using meta tags and title in a component

--- src/components/pages/404/404.jsx
+++ src/components/pages/404/404.jsx
@@ -7,6 +7,9 @@ const Page404 = () => {
   return (
       <div>
         <ErrorMessage/>
+        <meta name="description" content="This page is not found"/>
+        <title>This page is not found</title>
+
         <p className="error__message">Page doesn't exist</p>
         <Link className="error__message-back" to="/">Back to main page</Link>
       </div>
